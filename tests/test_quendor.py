5e7132cb7acb810e83799926a25d2f7472d325b4
Jeff Nyman <jeffnyman@gmail.com>
2023-08-08 02:55:31 -0500

    Adding expectations library to test framework.

--- tests/test_quendor.py
+++ tests/test_quendor.py
@@ -1,4 +1,7 @@
+from expects import expect, equal
+
+
 def test_package_version() -> None:
     from quendor import __version__
 
-    assert __version__ == "0.1.0"
+    expect(__version__).to(equal("0.1.0"))
