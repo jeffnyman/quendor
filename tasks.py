00b27062b4523d85d4cc54cbdd18b22a0183b9d8
Jeff Nyman <jeffnyman@gmail.com>
2023-08-08 03:03:29 -0500

    Adding code quality exception.

--- tasks.py
+++ tasks.py
@@ -4,19 +4,19 @@ from invoke import task
 @task
 def formatting(c):
     print("\n*** Formatting (with black) ***")
-    c.run("poetry run black src")
+    c.run("poetry run black src tests")
 
 
 @task
 def linting(c):
     print("\n*** Linting (with flake8) ***")
-    c.run("poetry run flake8 --count src")
+    c.run("poetry run flake8 --count src tests")
 
 
 @task
 def typing(c):
     print("\n*** Typing (with mypy) ***")
-    c.run("poetry run mypy src")
+    c.run("poetry run mypy src tests")
 
 
 @task
