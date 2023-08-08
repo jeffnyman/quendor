9b639f28fa77aa4d8c5359fc00ec1c9377b0d84f
Jeff Nyman <jeffnyman@gmail.com>
2023-08-08 03:23:56 -0500

    Adding cleanup task for generated artifacts.

--- tasks.py
+++ tasks.py
@@ -2,6 +2,24 @@ from invoke import task
 
 
 @task
+def clean(c):
+    print("\n *** Cleaning ***")
+
+    c.run(
+        "poetry run python -c \"import pathlib; [p.unlink() for p in pathlib.Path('.').rglob('*.py[co]') if not str(p).startswith('.venv')]\""
+    )
+    c.run(
+        "poetry run python -c \"import pathlib; [p.rmdir() for p in pathlib.Path('.').rglob('__pycache__') if not str(p).startswith('.venv')]\""
+    )
+    c.run(
+        "poetry run python -c \"import shutil; shutil.rmtree('./.pytest_cache', ignore_errors=True)\""
+    )
+    c.run(
+        "poetry run python -c \"import shutil; shutil.rmtree('./.mypy_cache', ignore_errors=True)\""
+    )
+
+
+@task
 def formatting(c):
     print("\n*** Formatting (with black) ***")
     c.run("poetry run black src tests")
