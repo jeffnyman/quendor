0ca6fa1eb68e510d49462554b4d49b93f7a81dbd
Jeff Nyman <jeffnyman@gmail.com>
2023-08-08 02:58:46 -0500

    Adding spec output to test framework.

--- tests/test_quendor.py
+++ tests/test_quendor.py
@@ -2,6 +2,8 @@ from expects import expect, equal
 
 
 def test_package_version() -> None:
+    """Current version is exposed on the package."""
+
     from quendor import __version__
 
     expect(__version__).to(equal("0.1.0"))
