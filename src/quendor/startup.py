e291e612235ac257339cf7ceea068c70d3b44ddd
Jeff Nyman <jeffnyman@gmail.com>
2023-08-09 03:24:30 -0500

    Adding minimum Python version check.

--- src/quendor/startup.py
+++ src/quendor/startup.py
@@ -1,7 +1,22 @@
+import sys
+
 from quendor import __version__
 
 
 def main() -> int:
     print(f"\nQuendor Z-Machine Interpreter (version: {__version__})\n")
 
+    check_python_version()
+
     return 0
+
+
+def check_python_version() -> None:
+    python_version = (
+        f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
+    )
+
+    if sys.version_info < (3, 8, 2):
+        sys.stderr.write("\nQuendor requires Python 3.8.2 or later.\n")
+        sys.stderr.write(f"Your current version is {python_version}\n\n")
+        sys.exit(1)
--- tests/test_quendor.py
+++ tests/test_quendor.py
@@ -1,5 +1,7 @@
 from expects import expect, equal, contain
 
+import pytest
+
 
 def test_package_version() -> None:
     """Current version is exposed on the package."""
@@ -22,3 +24,21 @@ def test_startup_banner(capsys) -> None:
 
     banner_text = f"Quendor Z-Machine Interpreter (version: {__version__})"
     expect(result).to(contain(banner_text))
+
+
+def test_bad_python_version(monkeypatch, capsys) -> None:
+    """Checks if Python version requirement is met."""
+
+    import sys
+    from quendor.startup import check_python_version
+
+    monkeypatch.setattr(sys, "version_info", (3, 7, 9))
+
+    with pytest.raises(SystemExit):
+        check_python_version()
+
+    captured = capsys.readouterr()
+    result = captured.err
+
+    error_text = "Quendor requires Python 3.8.2 or later."
+    expect(result).to(contain(error_text))
