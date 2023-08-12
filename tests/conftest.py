c04b665e16642370b265df6fcbded30ca9698b1c
Jeff Nyman <jeffnyman@gmail.com>
2023-08-12 10:12:33 -0500

    Adding tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,11 @@ import pytest
 
 
 @pytest.fixture()
+def zork1_z1(pytestconfig) -> Path:
+    return pytestconfig.rootdir / "tests" / "fixtures" / "zork1.z1"
+
+
+@pytest.fixture()
 def zork1_z3(pytestconfig) -> Path:
     return pytestconfig.rootdir / "tests" / "fixtures" / "zork1.z3"
 
--- tests/test_program.py
+++ tests/test_program.py
@@ -21,3 +21,17 @@ def test_read_valid_blorb(zork1_blorb) -> None:
 
     expect(program._data).to(be_a(bytes))
     expect(program._format.name).to(equal("BLORB"))
+
+
+def test_zcode_and_blorb_byte_equivalency(zork1_z1, zork1_blorb) -> None:
+    """Reads the same data whether blorbed or unblorbed."""
+
+    from quendor.program import Program
+
+    program = Program(zork1_z1)
+    zcode_data = program._data
+
+    program = Program(zork1_blorb)
+    blorb_data = program._data
+
+    expect(zcode_data).to(equal(blorb_data))
