d706d301adb5a8fef8d0fb26bc74a8ac4f7219a3
Jeff Nyman <jeffnyman@gmail.com>
2025-09-05 08:19:18 -0500

    Add bits and bytes content.

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,12 @@
 console.log("Ludic Historian");
 
 document.documentElement.classList.replace("no-js", "js");
+
+document.querySelectorAll("pre code").forEach((element) => {
+  let html = element.outerHTML;
+  let pattern = html.match(/\s*\n[\t\s]*/);
+
+  if (pattern) {
+    element.outerHTML = html.replace(new RegExp(pattern[0], "g"), "\n");
+  }
+});
